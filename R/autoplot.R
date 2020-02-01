7fe3572d7676a8e483ddf763cc1487729d4d1a90
bailliem <bailliem@gmail.com>
2020-02-01 20:30:09 +0100

    add first plot function

--- R/autoplot.R
+++ R/autoplot.R
@@ -7,18 +7,28 @@
 #'
 #' @examples
 #' library(survival)
+#' library(magrittr)
+#' library(dplyr)
+#' library(ggplot2)
+#' library(broom)
 #' survival::survfit(Surv(time, status) ~ sex, data = lung)%>%
-#' tidy() %>%
-#' autoplot.surv()
+#'   tidy() %>%
+#'   autoplot_surv()
 
-autoplot.surv <- function(df){
-  gg <- 
+autoplot_surv <- function(x) {
+  df <- x
+  gg <-
     df %>%
-    ggplot2::ggplot(aes(time, estimate, group = strata)) + 
+    ggplot2::ggplot(aes(time, estimate, group = strata)) +
     geom_line() +
     geom_linerange(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2) +
+    geom_point(
+      data = df %>% dplyr::filter(n.censor > 0),
+      aes(time, estimate),
+      alpha = 0.5
+    ) +
+    ggtitle("Update the title", subtitle = "Fill out the population") +
     facet_wrap( ~ strata)
   return(gg)
 }
-
 
